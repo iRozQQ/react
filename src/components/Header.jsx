b364aad9ebf6acdc9623900f13c08ee15b302cd4
iRozQQ <moidryz693@gmail.com>
2020-01-24 01:50:18 +0200

    react v 1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return ( <header className='header'>
+        <img src='https://cdn3.iconfinder.com/data/icons/picons-social/57/56-apple-512.png' />
+    </header>
+ )
+} 
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+      <div>
+      </div>
+      <img src='https://planetofhotels.com/blog/wp-content/uploads/Laguna-i-plyazh-Al%60-Mamzar.jpg'></img>
+        <div>
+          Ava+Description
+        </div>
+        <div>
+          My posts
+          <div>
+            New posts              
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+    </div>
+    );
+} 
+
+export default Profile;
